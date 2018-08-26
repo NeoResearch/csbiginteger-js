bd336c6bf2071d1745da72cbd728b27337dc4a6b
Igor Machado <igor.machado@gmail.com>
2018-08-26 01:22:45 -0300

    implementing basic methods and testing

--- csbiginteger.test.js
+++ csbiginteger.test.js
@@ -1,5 +1,29 @@
 const csBigInteger = require('./csbiginteger').csBigInteger;
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(new csBigInteger(1, 2).valueOf()).toBe(3);
+test('constructor csBigInteger() equals zero', () => {
+  expect(new csBigInteger().valueOf()).toBe(0);
 });
+
+test('constructor csBigInteger("123") equals 123', () => {
+  expect(new csBigInteger("123").valueOf()).toBe(123);
+});
+
+test('constructor csBigInteger(123) equals 123', () => {
+  expect(new csBigInteger(123).valueOf()).toBe(123);
+});
+
+test('constructor csBigInteger(127) equals 127', () => {
+  expect(new csBigInteger(127).valueOf()).toBe(127);
+});
+
+test('constructor csBigInteger(128) equals 128', () => {
+  expect(new csBigInteger(128).valueOf()).toBe(128);
+});
+
+test('constructor csBigInteger(-1) equals -1', () => {
+  expect(new csBigInteger(-1).valueOf()).toBe(-1);
+});
+
+//test('adds 1 + 2 to equal 3', () => {
+//  expect(new csBigInteger(1, 2).valueOf()).toBe(3);
+//});
