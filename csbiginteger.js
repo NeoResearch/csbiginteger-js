a3cb05c05588afdde20367400812f46389697172
Igor Machado <igor.machado@gmail.com>
2018-08-26 13:54:19 -0300

    storing data as native javascript number

--- csbiginteger.js
+++ csbiginteger.js
@@ -24,20 +24,17 @@
   > var n2 = csBigInteger(-1);   // Create a new <csBigInteger> with value -1
 	> var n3 = csBigInteger(n2);    // Return n2, unchanged (immutable)
 
-	The constructor form only takes a value and a sign. *n* must be a number, string or
+	The constructor form only takes a value *n* that must be a number, string or
 	bytearray of numbers (0..255) in big-endian order.
-  The second parameter sets the sign: -1 for
-	negative, +1 for positive, or 0 for zero. The array is *not copied and
+  The array is *not copied and
 	may be modified*. If the array contains only zeros, the sign parameter
 	is ignored and is forced to zero.
 
   > new csBigInteger([5]): create a new csBigInteger with value 5
-	> new csBigInteger([5], -1): create a new csBigInteger with value -5
   > var n2 = csBigInteger([127]);   // Create a new <csBigInteger> with value 127
   > var n2 = csBigInteger([128]);   // Create a new <csBigInteger> with value -128
   > new csBigInteger([251]): create a new csBigInteger with value -5 (fb)
   > new csBigInteger([251, 0]): create a new csBigInteger with value 251 (fb00)
-  > new csBigInteger([251, 0], -1): create a new csBigInteger with value -251 (fb00)
 
 	Parameters:
 
@@ -51,42 +48,38 @@
 
 		<parse>, <csBigInteger>
 */
-function csBigInteger(n, sign = 1) {
+function csBigInteger(n) {
     //console.log("n="+n);
+		//console.log("typeof n="+(typeof n));
+		//console.log(Object.prototype.toString.call(n));
 		if (n instanceof csBigInteger)
 			return n;
 		else if (typeof n === "undefined")
 			return ZERO;
-    else if (typeof n === "number") {
-      if(n >= 0)
-        return csBigInteger.parsePositiveInt10(n);
-      else
-        return csBigInteger.parseNegativeInt10(n);
-    }
-    else if (typeof n === "string") {
-      console.log("string:"+n);
-      return csBigInteger.parseStr(n);
-    }
-  // internal _data indicates byte array
-	this._data = n;
-	this._sign = sign;
+    //else if (typeof n === "number")
+		//	this._data = n;
+    else if ((typeof n === "string") || (Object.prototype.toString.call(n) === '[object Array]'))
+      return csBigInteger.parse(n);
+
+  //console.log("default assign n="+n);
+	this._data = n; // assign anyway (should be 'number')
 }
 
-csBigInteger._construct = function(n, sign) {
-	return new csBigInteger(n, sign);
+csBigInteger._construct = function(n) {
+	return new csBigInteger(n);
 };
 
-var ZERO = new csBigInteger([], 0);
+var ZERO = new csBigInteger(0);
 // Constant: ZERO
 // <csBigInteger> 0.
 csBigInteger.ZERO = ZERO;
 
-var ONE = new csBigInteger([1], 1);
+var ONE = new csBigInteger(1);
 // Constant: ONE
 // <csBigInteger> 1.
 csBigInteger.ONE = ONE;
 
-var M_ONE = new csBigInteger(ONE._data, -1);
+var M_ONE = new csBigInteger(-1);
 // Constant: M_ONE
 // <csBigInteger> -1.
 csBigInteger.M_ONE = M_ONE;
@@ -98,58 +91,6 @@ csBigInteger._0 = ZERO;
 // Constant: _1
 // Shortcut for <ONE>.
 csBigInteger._1 = ONE;
-
-/*
-	Constant: byte
-	Array of <csBigIntegers> from 0 to 255.
-
-	Very useful when you need a "byte" <csBigInteger>.
-
-	See Also:
-
-		<ZERO>, <ONE>, <_0>, <_1>
-*/
-csBigInteger.byte = [
-	ZERO,
-	ONE,
-	/* Assuming csBigInteger_base > 36 */
-	new csBigInteger( [2], 1),
-	new csBigInteger( [3], 1),
-	new csBigInteger( [4], 1),
-	new csBigInteger( [5], 1),
-	new csBigInteger( [6], 1),
-	new csBigInteger( [7], 1),
-	new csBigInteger( [8], 1),
-	new csBigInteger( [9], 1),
-	new csBigInteger([10], 1),
-	new csBigInteger([11], 1),
-	new csBigInteger([12], 1),
-	new csBigInteger([13], 1),
-	new csBigInteger([14], 1),
-	new csBigInteger([15], 1),
-	new csBigInteger([16], 1),
-	new csBigInteger([17], 1),
-	new csBigInteger([18], 1),
-	new csBigInteger([19], 1),
-	new csBigInteger([20], 1),
-	new csBigInteger([21], 1),
-	new csBigInteger([22], 1),
-	new csBigInteger([23], 1),
-	new csBigInteger([24], 1),
-	new csBigInteger([25], 1),
-	new csBigInteger([26], 1),
-	new csBigInteger([27], 1),
-	new csBigInteger([28], 1),
-	new csBigInteger([29], 1),
-	new csBigInteger([30], 1),
-	new csBigInteger([31], 1),
-	new csBigInteger([32], 1),
-	new csBigInteger([33], 1),
-	new csBigInteger([34], 1),
-	new csBigInteger([35], 1),
-	new csBigInteger([36], 1)
-  // TODO: continue until 255: ff00
-];
 
 
 /*
@@ -172,25 +113,19 @@ csBigInteger.prototype.toString = function(base=10) {
   if (!((base == 2) || (base == 10) || (base == 16))) {
 		throw new Error("illegal radix " + base + ".");
 	}
-	if (this._sign === 0) {
-		return "0";
+
+	if (base === 10)
+	  return this._data.toString();
+
+	var shex = "";
+	for (var i = this._data.length - 1; i >= 0; i--) {
+		var sbyte = this._data[i].toString(16);
+		if(sbyte.length < 2)
+			sbyte = "0"+sbyte;
+		shex += sbyte;
 	}
 
-  var shex = "";
-  for (var i = this._data.length - 1; i >= 0; i--) {
-    var sbyte = this._data[i].toString(16);
-    if(sbyte.length < 2)
-      sbyte = "0"+sbyte;
-    shex += sbyte;
-  }
-
-	if (base === 10) {
-    // decimal
-		var str = this._sign < 0 ? "-" : "";
-    str += parseInt(shex, 16).toString();
-		return str;
-	}
-  else if(base === 16) {
+  if(base === 16) {
     // hexstring
     return shex;
   }
@@ -202,8 +137,8 @@ csBigInteger.prototype.toString = function(base=10) {
 
 
 /*
-	Function: parseStr
-	Parse a string into a <csBigInteger>.
+	Function: parse
+	Parse a string or byte array into a <csBigInteger>.
 
 	- "0x" or "0X": *base* = 16
 	- "0_": *base* = 2
@@ -218,47 +153,58 @@ csBigInteger.prototype.toString = function(base=10) {
 
 		a <csBigInteger> instance.
 */
-csBigInteger.parseStr = function(s, base = 10) {
-	s = s.toString();
-
-	var prefixRE;
-  if (base == 16) {
-		prefixRE = '0x';
-	}
-	else if (base == 2) {
-		prefixRE = '0_';
-	}
-	else {
-		prefixRE = '';
+csBigInteger.parse = function(n, base = 10) {
+	if(Object.prototype.toString.call(n) === '[object Array]') {
+	  s = "0x";
+		for(i=0;i<n.length;i++) {
+			var dig = n[i].toString(16);
+			if(dig.length==1)
+				dig = "0"+dig;
+			s = s + dig;
+		}
+		return csBigInteger.parse(s, 16);
 	}
 
-  //var sign = (s[0] == "-")? -1 : 1;
+	var s = n.toString();
 
-  // default is 10
-  var num = parseInt(s, 10);
+  var num = parseInt(s, base);
 
-  return new csBigInteger(num, 1);
+  return new csBigInteger(num);
 };
 
 /*
-	Function: parsePositiveInt10
-	Parse a positive (or zero) base-10 number into a <csBigInteger>.
-
-	Parameters:
-
-		n - The base-10 number to parse.
+	Function: toByteArray
+	Converts the biginteger value to big-endian byte array
 
 	Returns:
 
-		a <csBigInteger> instance.
+		a big-endian byte array instance.
 */
-csBigInteger.parsePositiveInt10 = function(intvalue) {
+csBigInteger.toByteArray = function() {
+	//console.log("toByteArray = "+this._data);
+	if(this._data >= 0)
+		return this.positiveToByteArray();
+	else
+		return this.negativeToByteArray();
+};
+
+
+/*
+	Function: positiveToByteArray
+	Parse a positive (or zero) base-10 number into a <csBigInteger>.
+
+	Returns:
+
+    a big-endian byte array
+*/
+csBigInteger.positiveToByteArray = function() {
+	var intvalue = this._data;
   // big-endian byte array
   var data = [];
   // guarantee value is non-negative
   if(intvalue < 0)
      intvalue = 0;
-  console.log("intvalue="+intvalue);
+  //console.log("intvalue="+intvalue);
   // int to hexstring
   var hval = intvalue.toString(16);
   // padding
@@ -268,30 +214,27 @@ csBigInteger.parsePositiveInt10 = function(intvalue) {
   for (var i = 0; i < hval.length - 1; i += 2)
       data.push(parseInt(hval.substr(i, 2), 16));
 
-  return new csBigInteger(data, 1);
+  return data;
 };
 
 
 /*
-	Function: parseNegativeInt10
+	Function: negativeToByteArray
 	Parse a negative base-10 number into a <csBigInteger>.
-
-	Parameters:
-
-		n - The base-10 number to parse.
 
 	Returns:
 
-		a <csBigInteger> instance.
+		a big-endian byte array.
 */
-csBigInteger.parseNegativeInt10 = function(intvalue) {
+csBigInteger.negativeToByteArray = function() {
+	var intvalue = this._data;
   // big-endian byte array
   var data = [];
   // guarantee value is non-negative
   if(intvalue >= 0)
      return ZERO;
-     
-  console.log("intvalue="+intvalue);
+
+  //console.log("intvalue="+intvalue);
   // int to hexstring
   var hval = intvalue.toString(16);
   // padding
@@ -301,7 +244,7 @@ csBigInteger.parseNegativeInt10 = function(intvalue) {
   for (var i = 0; i < hval.length - 1; i += 2)
       data.push(parseInt(hval.substr(i, 2), 16));
 
-  return new csBigInteger(data, 1);
+  return data;
 };
 
 
