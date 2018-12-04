a6e813e44c47fae50761df8ff32a2d64fc34ba20
Igor Machado <igor.machado@gmail.com>
2018-12-04 12:36:46 -0200

    migrated functions to BN

--- csBigInteger.js
+++ csBigInteger.js
@@ -344,19 +344,76 @@ csBigInteger.prototype.toHexString = function() {
 		 return bihex;
 	}
 	else
-		 return csBigInteger.negbigint2lehex(bigint);
+	{
+		 // negative numbers
+		 //console.log("NEGATIVE: "+bigint);
+
+		 /*
+		 // https://msdn.microsoft.com/en-us/library/system.numerics.biginteger(v=vs.110).aspx
+		 The BigInteger structure assumes that negative values are stored by using two's complement representation. Because the BigInteger structure represents a numeric value with no fixed length, the BigInteger(Byte[]) constructor always interprets the most significant bit of the last byte in the array as a sign bit. To prevent the BigInteger(Byte[]) constructor from confusing the two's complement representation of a negative value with the sign and magnitude representation of a positive value, positive values in which the most significant bit of the last byte in the byte array would ordinarily be set should include an additional byte whose value is 0. For example, 0xC0 0xBD 0xF0 0xFF is the little-endian hexadecimal representation of either -1,000,000 or 4,293,967,296. Because the most significant bit of the last byte in this array is on, the value of the byte array would be interpreted by the BigInteger(Byte[]) constructor as -1,000,000. To instantiate a BigInteger whose value is positive, a byte array whose elements are 0xC0 0xBD 0xF0 0xFF 0x00 must be passed to the constructor.
+		 */
+		 //x=-1000000; // must become (big endian) "f0bdc0" => little endian C0 BD F0  (careful with positive 4,293,967,296 that may become negative, need to be C0 BD F0 FF 00)
+		 // ASSERT (x < 0) !!! x==0 is problematic! equals to 256...
+		 //x=-1; // ff => 0x ff
+		 //x=-2; // fe => 0x fe
+		 //x=-127; // 81 => 0x 81
+		 //x=-255; // "ff01" => 0x 01 ff
+		 //x=-256; // "ff00" => 0x 00 ff
+		 //x=-257; // "feff" => 0x ff fe
+		 //x=-128; // "ff80" => 0x 80 ff
+		 // only for negative integers
+		 var x = bigint.mul(new BN(-1)); // turn into positive
+		 //console.log("POSITIVE:" +x);
+		 // ========================
+		 // perform two's complement
+		 // ========================
+		 // convert to binary
+		 var y = x.toString(2);
+		 //console.log("BINARY:" +y);
+		 //console.log("FIRST BINARY: "+y);
+		 // extra padding for limit cases (avoid overflow)
+		 y = "0"+y;
+		 //guarantee length must be at least 8, or add padding!
+		 while((y.length<8) || (y.length % 8 != 0)) {
+				//console.log("ADDING PADDING 1!");
+				y = "0"+y;
+		 }
+		 //console.log("BINARY AFTER PADDING:" +y);
+		 // invert bits
+		 var y2 = "";
+		 for(var i = 0; i<y.length; i++)
+				y2 += y[i]=='0'?'1':'0';
+		 //console.log("SECOND BINARY: "+y2);
+		 // go back to int
+		 var y3 = new BN(y2, 2);
+		 //console.log("INT is "+y3);
+		 // sum 1
+		 y3 = y3.add(new BN(1));
+		 //console.log("INT is after sum "+y3);
+		 // convert to binary again
+		 var y4 = y3.toString(2);
+		 //guarantee length must be at least 8, or add padding!
+		 while(y4.length < 8) {
+				//console.log("ADDING PADDING!");
+				y4 = "0"+y4;
+		 }
+		 // convert to big-endian hex string
+		 var y5 = new BN(y4,2).toString(16);
+		 // convert to little-endian
+		 return csBigInteger.revertHexString(y5);
+	}
 };
 
-
+/*
 // negative big integers returned as (big-endian) hex
 csBigInteger.negbigint2lehex = function (intvalue) {
    if(intvalue >= 0) // ONLY NEGATIVE!
       return null;
    var x = intvalue;
-   /*
+
    // https://msdn.microsoft.com/en-us/library/system.numerics.biginteger(v=vs.110).aspx
-   The BigInteger structure assumes that negative values are stored by using two's complement representation. Because the BigInteger structure represents a numeric value with no fixed length, the BigInteger(Byte[]) constructor always interprets the most significant bit of the last byte in the array as a sign bit. To prevent the BigInteger(Byte[]) constructor from confusing the two's complement representation of a negative value with the sign and magnitude representation of a positive value, positive values in which the most significant bit of the last byte in the byte array would ordinarily be set should include an additional byte whose value is 0. For example, 0xC0 0xBD 0xF0 0xFF is the little-endian hexadecimal representation of either -1,000,000 or 4,293,967,296. Because the most significant bit of the last byte in this array is on, the value of the byte array would be interpreted by the BigInteger(Byte[]) constructor as -1,000,000. To instantiate a BigInteger whose value is positive, a byte array whose elements are 0xC0 0xBD 0xF0 0xFF 0x00 must be passed to the constructor.
-   */
+   //The BigInteger structure assumes that negative values are stored by using two's complement representation. Because the BigInteger structure represents a numeric value with no fixed length, the BigInteger(Byte[]) constructor always interprets the most significant bit of the last byte in the array as a sign bit. To prevent the BigInteger(Byte[]) constructor from confusing the two's complement representation of a negative value with the sign and magnitude representation of a positive value, //positive values in which the most significant bit of the last byte in the byte array would ordinarily be set should include an additional byte whose value is 0. For example, 0xC0 0xBD 0xF0 0xFF is the little-endian hexadecimal representation of either -1,000,000 or 4,293,967,296. Because the most significant bit of the last byte in this array is on, the value of the byte array would be interpreted by the BigInteger(Byte[]) constructor as -1,000,000. To instantiate a BigInteger whose value //is positive, a byte array whose elements are 0xC0 0xBD 0xF0 0xFF 0x00 must be passed to the constructor.
+
    //x=-1000000; // must become (big endian) "f0bdc0" => little endian C0 BD F0  (careful with positive 4,293,967,296 that may become negative, need to be C0 BD F0 FF 00)
    // ASSERT (x < 0) !!! x==0 is problematic! equals to 256...
    //x=-1; // ff
@@ -415,6 +472,7 @@ csBigInteger.negbigint2lehex = function (intvalue) {
 
    return csBigInteger.revertHexString(y5); // big endian
 }
+*/
 
 /*
 	Function: valueOf
